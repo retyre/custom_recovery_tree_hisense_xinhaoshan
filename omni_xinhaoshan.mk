66cedae412cfdf95c805cf9c944af3bf244ced28
Re Tyre <grexer@gmail.com>
2025-01-20 16:16:08 -0500

    Update omni_xinhaoshan.mk

--- omni_xinhaoshan.mk
+++ omni_xinhaoshan.mk
@@ -16,6 +16,16 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from gta9pwifi device
+$(call inherit-product, device/samsung/gta9pwifi/device.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := xinhaoshan
 PRODUCT_NAME := omni_xinhaoshan
